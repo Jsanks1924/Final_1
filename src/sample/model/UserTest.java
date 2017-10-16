7ef60cedd11f34b48b4df8e4da899ced63743525
Jsanks1924 <31380394+Jsanks1924@users.noreply.github.com>
2017-10-16 00:09:04 -0400

    final project part 2 i decided to implement hashmap because it doesnt store duplicates and efficient to search

---
+++ src/sample/model/UserTest.java
@@ -0,0 +1,26 @@
+package sample.model;
+
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+
+    @org.junit.Test
+    public void equals() throws Exception {
+    }
+
+    @org.junit.Test
+    public void validateEmail() throws Exception {
+    }
+
+    @org.junit.Test
+    public void validateFirst() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void confirmPassword() throws Exception {
+    }
+
+}
